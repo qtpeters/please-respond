66b4b205520f737bd7ca26ead757dc2987404230
qtpeters <quinn.t.peters@gmail.com>
2021-05-01 20:59:23 -0400

    Making my tasks look nicer

--- src/tasks.py
+++ src/tasks.py
@@ -4,16 +4,31 @@ from invoke import task
 
 @task
 def build( c ):
+
+    """
+    Build the software and create an executable for distribution.
+    """
+
     print("Building Please Respond...")
     c.run( "pyinstaller -y please_respond.py" )
 
 @task
 def clean( c ):
+
+    """
+    Remove unversioned artifacts.
+    """
+
     rmtree( "dist" )
     rmtree( "build" )
     rmtree( "__pycache__" )
 
 @task(build)
 def run( c, seconds="" ):
+
+    """
+    Execute the code.
+    """
+
     cmd = "./dist/please_respond/please_respond %s" % seconds
     c.run( cmd )
